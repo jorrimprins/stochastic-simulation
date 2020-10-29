6bd03fe2e2af4d01e1a0d00ef753e6cbdf21e605
jorrimprins <71085057+jorrimprins@users.noreply.github.com>
2020-10-29 16:48:16 +0100

    Update to contain function create_set

--- code/simulate.py
+++ code/simulate.py
@@ -1,4 +1,10 @@
-import numpy as np
+def create_set(c,N):
+    if not type(c) is complex:
+        raise TypeError("Only complex numbers allowed")
+    z = [0]
+    for i in range(N):
+        z.append(z[i]**2 + c)
+    return z
 
-def iterate(c):
-  return c^2
+print(create_set(complex(1,3),10))
+
