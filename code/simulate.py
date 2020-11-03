80c8169d4ac740783d907ed7f72ac6b58a01b3d4
jorrimprins <71085057+jorrimprins@users.noreply.github.com>
2020-11-03 10:31:44 +0100

    Add test commit

--- code/simulate.py
+++ code/simulate.py
@@ -1,10 +1,11 @@
-def create_set(c,N):
+def create_set(c, n):
     if not type(c) is complex:
         raise TypeError("Only complex numbers allowed")
     z = [0]
-    for i in range(N):
+    for i in range(n):
         z.append(z[i]**2 + c)
     return z
 
-print(create_set(complex(1,3),10))
-
+print(create_set(complex(1, 3), 10))
+print('hallo')
+print('hoi')
