22f7775d664d1c2b245f627946be7c7d514df293
jorrimprins <71085057+jorrimprins@users.noreply.github.com>
2020-11-03 10:59:23 +0100

    Change workspace

--- code/simulate.py
+++ code/simulate.py
@@ -3,8 +3,9 @@ def create_set(c, n):
         raise TypeError("Only complex numbers allowed")
     z = [0]
     for i in range(n):
-        z.append(z[i]**2 + c)
+        z.append(z[i] ** 2 + c)
     return z
+
 
 print(create_set(complex(1, 3), 10))
 print('hallo')
