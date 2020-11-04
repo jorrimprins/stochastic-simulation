c29a7952c5fb8f2657d3d8f9b414c5693636a241
jorrimprins <71085057+jorrimprins@users.noreply.github.com>
2020-11-04 18:35:36 +0100

    Plot function for mandelbrot (also change ws and import packages)

--- code/simulate.py
+++ code/simulate.py
@@ -1,12 +1,48 @@
-def create_set(c, n):
-    if not type(c) is complex:
+from numba import jit
+import numpy as np
+import seaborn as sns
+import matplotlib as plt
+colormap = sns.color_palette("magma", as_cmap=True)
+
+@jit
+def iterate(c, n):
+
+    # Iterates complex number n times, or until the iteration shows divergence.
+    # Outputs the number of iterations until divergence or 0 for numbers in mandelbrot set.
+    if not isinstance(c, complex):
         raise TypeError("Only complex numbers allowed")
-    z = [0]
+    z = 0
     for i in range(n):
-        z.append(z[i] ** 2 + c)
-    return z
+        if abs(z) > 2:
+            return i
+        z = z ** 2 + c
+    return 0
+
+@jit
+def set_mb(range_real, range_im, size, n):
+    ## Creates set for given ranges, range_real, range_im and size are tuples.
+    range_real = np.linspace(range_real[0],range_real[1],size[0])
+    range_im = np.linspace(range_im[0],range_im[1],size[1])
+    mb_set = np.zeros(size)
+
+    for i in range(size[0]):
+        for j in range(size[1]):
+            mb_set[i, j] = iterate(complex(range_real[i], range_im[j]), n)
+    return mb_set
+
+@jit
+def view_mb(range_real, range_im, size, n, dpi=100):
+    plotsize = [i * dpi for i in size]
+    mb_set = set_mb(range_real, range_im, plotsize, n)
+
+    fig = plt.figure(figsize=size, dpi=dpi)
+    #ticks = np.arange(0, img_width, 3 * dpi)
+    #x_ticks = xmin + (xmax - xmin) * ticks / img_width
+    #plt.xticks(ticks, x_ticks)
+    #y_ticks = ymin + (ymax - ymin) * ticks / img_width
+    #plt.yticks(ticks, y_ticks)
+    #norm = colors.PowerNorm(0.1)
+    fig.plot(mb_set.T, cmap=colormap)#, origin='lower', norm=norm)
+    fig.show()
 
 
-print(create_set(complex(1, 3), 10))
-print('hallo')
-print('hoi')
