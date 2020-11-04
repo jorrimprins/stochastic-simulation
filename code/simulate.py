d9c858a0fa4a08283de707118c717af7d69dc4f7
jorrimprins <71085057+jorrimprins@users.noreply.github.com>
2020-11-05 00:05:43 +0100

    Set right colors

--- code/simulate.py
+++ code/simulate.py
@@ -1,11 +1,14 @@
 from numba import jit
 import numpy as np
 import seaborn as sns
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
 sns.set_style("darkgrid")
 
 @jit
-def iterate(c, n):
+def iterate(c, n=100):
 
     # Iterates complex number n times, or until the iteration shows divergence.
     # Outputs the number of iterations until divergence or 0 for numbers in mandelbrot set.
@@ -19,7 +22,7 @@ def iterate(c, n):
     return 0
 
 @jit
-def set_mb(range_real, range_im, size, n):
+def set_mb(range_real, range_im, size, n=100):
     ## Creates set for given ranges, range_real, range_im and size are tuples.
     range_real = np.linspace(range_real[0],range_real[1],size[0])
     range_im = np.linspace(range_im[0],range_im[1],size[1])
@@ -31,18 +34,18 @@ def set_mb(range_real, range_im, size, n):
     return mb_set
 
 @jit
-def view_mb(range_real, range_im, size, n, dpi=100):
+def view_mb(range_real, range_im, size, n=100, dpi=100):
     plotsize = [i * dpi for i in size]
     mb_set = set_mb(range_real, range_im, plotsize, n)
 
-    fig = plt.figure(figsize=size, dpi=dpi)
+    plt.figure(figsize=size, dpi=dpi)
     #ticks = np.arange(0, img_width, 3 * dpi)
     #x_ticks = xmin + (xmax - xmin) * ticks / img_width
     #plt.xticks(ticks, x_ticks)
     #y_ticks = ymin + (ymax - ymin) * ticks / img_width
     #plt.yticks(ticks, y_ticks)
-    #norm = colors.PowerNorm(0.1)
-    fig.plot(mb_set.T,cmap='magma')#, origin='lower', norm=norm)
-    fig.show()
+    norm = colors.PowerNorm(0.1)
+    plt.plot(mb_set.T, cmap='gnuplot2', origin='lower', norm=norm)
+    plt.show()
 
-
+view_mb((-1.5,.25),(-.8,.8),(1,1),100)
